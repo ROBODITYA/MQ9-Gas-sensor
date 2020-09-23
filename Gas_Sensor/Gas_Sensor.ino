4d130c0df32056788cf9f6e506e0e6052e218e4d
ROBODITYA <aadityauti773@gmail.com.com>
2020-09-23 11:18:21 +0530

    This is Arduino based MQ9 Gas detection system with alarm.

---
+++ Gas_Sensor/Gas_Sensor.ino
@@ -0,0 +1,41 @@
+  # define GREEN 2
+  # define ORANGE 3
+  # define RED 4
+  # define Buzzer 5
+  # define Sensor A0
+    
+  void setup() {
+    for (int i = 2; i < 6; i++)
+      pinMode(i, OUTPUT);
+  
+    Serial.begin(9600);
+  }
+  
+  void loop() {
+    
+    for (int i = 2; i < 6; i++)
+      digitalWrite(i, LOW);
+  
+    status_Indicator();
+  
+  }
+  
+  void status_Indicator()
+  {
+    int sensor_In = analogRead(A0);
+    Serial.println(sensor_In);
+    
+    if ( sensor_In >=40  )
+    {
+      digitalWrite(RED, HIGH);
+      digitalWrite(Buzzer,HIGH);
+    }
+    else if (sensor_In >=25 )
+    {
+      digitalWrite(ORANGE, HIGH);
+    } else
+    {
+      digitalWrite(GREEN, HIGH);
+    }
+    delay(10);
+  }
